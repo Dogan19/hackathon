d45279ba845650047784b93fafdd2142c6275942
Hunaee <67273688+Hunaee@users.noreply.github.com>
2025-02-04 16:19:42 +0100

    home page /menu App.js

--- src/App.js
+++ src/App.js
@@ -6,15 +6,30 @@ import Contact from "./pages/contact";
 function App() {
   return (
     <Router>
-      <link href="https://cdn.jsdelivr.net/npm/tailwindcss@latest/dist/tailwind.min.css" rel="stylesheet" />
-      <nav className="nav-bar">
-        <ul>
-          <li><Link to="/">Accueil</Link></li>
-          <li><Link to="/blog">Blog</Link></li>
-          <li><Link to="/vente">En vente</Link></li>
-          <li><Link to="/expert">Trouver un expert</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-
+      <link
+        href="https://cdn.jsdelivr.net/npm/tailwindcss@latest/dist/tailwind.min.css"
+        rel="stylesheet"
+      />
+      <nav className="flex nav-bar justify-between px-8 pt-4 pb-4 border-b">
+        <div className="flex items-center bg-gray-800">
+          <img src="./assets/img/logo.jpg"></img>
+        </div>
+        <ul className="flex gap-4 justify-end bg-[#AAAAAA]">
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/vente">En vente</Link>
+          </li>
+          <li>
+            <Link to="/expert">Trouver un expert</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
       </nav>
       <Routes>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,22 @@
-
-
+import React from "react";
+import Slider from "../components/slider";
 const Home = () => {
+  return (
+    <>
+      <div className="flex p-8">
+        <div className="w-1/5 flex flex-col justify-center  items-center">
+          <p className="text-3xl font-bold">Mon super slogan</p>
+          <button className="w-32 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-5/5" style={{marginTop:"100px", width:"10rem"}}>
+            Click Me
+          </button>
+        </div>
 
-    return(
-        <header>
-            <button>Home</button>
-            <button>Blog</button>
-            <button>Listing</button>
-            <button>Event</button>
-            <button>Contact</button>
-        </header>
-    )
-}
+        <div className="w-4/5 pl-32">
+          <Slider />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
