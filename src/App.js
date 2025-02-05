33bf7146b71dd7e849acca744bd06c650c9482d7
Dogan <dozkara@student.efp.be>
2025-02-05 15:31:32 +0100

    Suppression des 2 logo

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
+import Listing from "./pages/listing";
+import Menu from "./pages/menu";
 
 function App() {
   return (
     <Router>
-      <link
-        href="https://cdn.jsdelivr.net/npm/tailwindcss@latest/dist/tailwind.min.css"
-        rel="stylesheet"
-      />
-      <nav className="flex nav-bar justify-between px-8 pt-4 pb-4 border-b">
-        <div className="flex items-center bg-gray-800">
-          <img src="./assets/img/logo.jpg"></img>
-        </div>
-        <ul className="flex gap-4 justify-end bg-[#AAAAAA]">
-          <li>
-            <Link to="/">Accueil</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link to="/vente">En vente</Link>
-          </li>
-          <li>
-            <Link to="/expert">Trouver un expert</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/expert" element={<Listing />} />
+
         <Route path="/contact" element={<Contact />} />
+        <Route path="/menu" element={<Menu />} />
+
       </Routes>
     </Router>
   );
